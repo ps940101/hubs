909b446ee81138fb3eac600a87bce1be778ebb8f
Manuel Martin <manmartin@mozilla.com>
2022-03-02 19:04:22 +0100

    Fix hand raised event name

--- src/hub.js
+++ src/hub.js
@@ -1178,7 +1178,7 @@ document.addEventListener("DOMContentLoaded", async () => {
       permissions: meta.permissions,
       streaming: meta.streaming,
       recording: meta.recording,
-      handRaised: meta.hand_raised,
+      hand_raised: meta.hand_raised,
       typing: meta.typing
     });
   });
@@ -1247,7 +1247,7 @@ document.addEventListener("DOMContentLoaded", async () => {
       permissions: current.permissions,
       streaming: current.streaming,
       recording: current.recording,
-      handRaised: current.hand_raised,
+      hand_raised: current.hand_raised,
       typing: current.typing
     });
   });
